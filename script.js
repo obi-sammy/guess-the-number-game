af78200ee1b505c91c31783ba23f527c833b9055
Samuel Obi <samobi588@gmail.com>
2023-06-06 08:31:39 +0100

    added eventlistener to checkBtn and wrote the onClick function that would update the UI

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ const highScoreEL = document.querySelector(".highscore");
 const message = document.querySelector(".message");
 const body =  document.querySelector(".body");
 const number = document.querySelector(".number");
-
+const checkBtn = document.getElementById('check-btn');
 
 //Generating random number
 const randomNumber = Math.floor(Math.random() * 100)+1;
+
+// used this to test if randomNumber is correct
+console.log(randomNumber)
 
 //assigning random number to the hidden number element
 number.value = randomNumber;
@@ -22,6 +25,22 @@ number.value = randomNumber;
 scoreEL.textContent = score;
 highScoreEL.textContent = highScore;
 
+//adding eventlistener that listens for a click and runs the onClick() function
+checkBtn.addEventListener('click', onClick)
 
+function onClick() {
+    //grabs the guessed number from the input field
+    const guessedNumber = document.getElementById('guess-number-input').valueAsNumber
+
+    //checks if the guessed number is equals to the random number and updates the UI
+    if(guessedNumber === randomNumber) {
+        body.className += ' background-green'
+        message.textContent = '🎉Correct Number!'
+
+        if(highScoreEL.textContent < scoreEL.textContent){
+            highScoreEL.textContent = scoreEL.textContent
+        }
+    }
+}
 
 
